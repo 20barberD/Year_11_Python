506550f48d947e0b07c840d6317c31d525cf02b5
20BarberD <20BarberD@birchwoodhigh.org.uk>
2024-11-22 15:56:30 +0000

    numbers.py was feeling silly

--- Numbers.py
+++
@@ -1,4 +0,0 @@
-def main():
-    number1 = float(input("Enter a number: "))
-    number2 = float(input("Enter another number: "))
-    print(number1 + number2)
--- numbers.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter another number: "))
-if number1 > number2:
-    print(number1)
-elif number2 > number1:
-    print(number2)
-else:
-    print("Both numbers are equal! ")
